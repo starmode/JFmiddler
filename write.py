4bb047b4354f2e91682c0f884fe8cb51cf94419c
starmode <kind@mail.ustc.edu.cn>
2017-06-16 21:18:56 +0800

    first commit -half of information extract

--- write.py
+++ write.py
@@ -9,9 +9,7 @@ with open('enbins.i', 'w') as f:
     f.write('\'' + neutron.name + '\'\n')
     f.write(str(neutron.ene_num) + '\n')
     ele_inf = list(','.join(neutron.ene_bin))
-    print(ele_inf)
     index = [i for i, a in enumerate(ele_inf) if a == ',']
-    print(index)
     for i in range(1, len(index) // 7 + 1):
         ele_inf.insert(index[7 * i - 1] + i, '\n')
     f.write(''.join(ele_inf))
