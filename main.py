eee9aeaa4bbf4a00b78cfc87793e4b0ce44c4989
Junyu Chen <kind@mail.ustc.edu.cn>
2018-02-25 16:22:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
 
-from window import Dynamics
-from PyQt5.QtWidgets import QApplication
 import cgitb
+from PyQt5.QtWidgets import QApplication
+from window import Dynamics
 
 if __name__ == "__main__":
     import sys
